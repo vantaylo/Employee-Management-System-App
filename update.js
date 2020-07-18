32ba3e346a684364582cbd21a583a680df18db70
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-07-18 15:09:55 -0500

    update code for manager and role

--- update.js
+++ update.js
@@ -14,15 +14,83 @@ connection.connect(function (err) {
   if (err) throw err;
 });
 
-var update = function () {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "picked_update",
-      message: "What would you like to update?",
-      choices: ["Employees", "Roles", "Departments"],
-    },
-  ]);
+var update = async function () {
+  var allEmployees;
+  var employeeId;
+
+  await connection.query("SELECT * FROM employees_db.employee_info", function (
+    err,
+    res
+  ) {
+    if (err) throw err;
+
+    allEmployees = res.map(function (val, index, _) {
+      return (
+        val.id + " - " + val.employee_first_name + " " + val.employee_last_name
+      );
+    });
+
+    console.log("RES: ", res);
+    console.log("ALL EMPLOYEES: ", allEmployees);
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "picked_update_employee",
+          message: "Which employee would you like to update?",
+          choices: allEmployees,
+        },
+        {
+          type: "list",
+          name: "picked_update_choices",
+          message: "What would you like to update?",
+          choices: ["Employee's Role", "Employee's Manager"],
+        },
+      ])
+      .then((answers) => {
+        employeeId = answers.picked_update_employee.split(" ")[0];
+
+        if (answers.picked_update_choices === "Employee's Role") {
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                name: "picked_update_role",
+                message: "What is their new role?",
+              },
+            ])
+            .then((answers) => {
+              connection.query(
+                "UPDATE employee_info SET employee_role = ? WHERE id = ?",
+                [answers.picked_update_role, employeeId],
+                function (err, res) {
+                  if (err) throw err;
+                }
+              );
+            });
+        } else if (answers.picked_update_choices === "Employee's Manager") {
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "picked_update_manager",
+                message: "Who is their new manager?",
+                choices: ["Manager A", "Manager B", "Manager C", "Manager D"],
+              },
+            ])
+            .then((answers) => {
+              connection.query(
+                "UPDATE employee_info SET employee_manager = ? WHERE id = ?",
+                [answers.picked_update_manager, employeeId],
+                function (err, res) {
+                  if (err) throw err;
+                }
+              );
+            });
+        }
+      });
+  });
 };
 
 module.exports = update;
